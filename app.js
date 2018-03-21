1b26d848f87b3daea63a66ffecc3d2ebc6b1764d
Kenneth Duverge <Kenneth.Duverge1@gmail.com>
2018-03-21 15:31:41 -0400

    refactored code

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ let celciusInput = document.querySelector("#celcius > input");
 let farenheightInput = document.querySelector("#farenheight > input");
 let kelvinInput = document.querySelector("#kelvin > input");
 
-function roundNum(num){
-    return Math.round(num * 100)/100;
-}
+var roundNum = num => Math.round(num * 100)/100;
 
-function cToFtoKelvin(){
+var cToFtoKelvin = () => {
     const cTemp = parseFloat(celciusInput.value);
     const fTemp = (cTemp * (9/5) + 32);
     const kTemp = cTemp + 273.15;
@@ -14,7 +12,7 @@ function cToFtoKelvin(){
     kelvinInput.value = roundNum(kTemp);
 }
 
-function ftoCtoKelvin() {
+var ftoCtoKelvin = () => {
     const fTemp = parseFloat(farenheightInput.value);
     const cTemp = (fTemp - 32) * (5/9);
     const kTemp = (fTemp + 459.67) * (5/9);
@@ -22,19 +20,19 @@ function ftoCtoKelvin() {
     kelvinInput.value = roundNum(kTemp);
 }
 
-function kToFtoCelcius() {
+var kToFtoCelcius = () => {
     const kTemp = parseFloat(kelvinInput.value);
     const cTemp = kTemp - 273.15;
     const fTemp = 9/5 * (kTemp - 273) + 32;
     celciusInput.value = roundNum(cTemp);
     farenheightInput.value = roundNum(fTemp);
-
 }
 
-function main(){
+var main = () => {
     celciusInput.addEventListener("input", cToFtoKelvin);
     farenheightInput.addEventListener("input", ftoCtoKelvin);
     kelvinInput.addEventListener("input", kToFtoCelcius);
 }
+
 
 main();
